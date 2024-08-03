15afe2888ef88d26ce4da1fadd1239a5b5838625
HoaThu25 <hoathu.nt.hec@gmail.com>
2024-08-03 17:53:00 +0700

    btvn buoi 1

--- Buoi4/bai5.py
+++
@@ -1,9 +0,0 @@
-n=int(input("Nhap n phan tu cho xau cua ban: "))
-a=[input(f'a[{i}]=')  for i in range(n)]
-b=tuple(a)
-print("Tuple b:",b)
-count=0
-for i in a:
-    if i.isdigit():
-        count+=1
-print("Số phần tử có dạng số trong tuple b là:",count)
